f8229cab0b2c1fa937c49a1a48858ffb2779db98
Philip Ingram <philip.d.ingram@gmail.com>
2016-01-21 23:51:05 -0300

    remove carousel, update copy on first page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
-   'use strict';	
- 
+   'use strict';
+
    /* ==============================================
 		MODERNIZR
 	=============================================== */
@@ -13,21 +13,21 @@ jQuery(document).ready(function($) {
 			}
 		}
 	});
-	
+
 	/* ==============================================
 		PULSE FALLBACK
 	=============================================== */
 	if(!Modernizr.cssanimations) {
 		$('.btn').addClass('btn-noanimated');
 	}
-	
+
    /* ==============================================
 		WAYPOINTS
-	=============================================== */	
-	
+	=============================================== */
+
 	// Only load parallax when not on mobile devices
 	if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-		
+
 		$('.animated').waypoint(function() {
 			$(this).each(function(){
 				var animation = $(this).attr( "data-animation" );
@@ -39,14 +39,14 @@ jQuery(document).ready(function($) {
 			offset: '70%',
 			triggerOnce: true
 		});
-	
+
 	} else {
-		
+
 		$('.animated').addClass( 'visible' );
-	
+
 	}
-	
-	$('#about').waypoint(function() {		
+
+	$('#about').waypoint(function() {
 			$('#about .progress-bar').each(function(){
 				var percent = $(this).attr( "data-value" ) + '%';
 				$(this).css( "width", percent );
@@ -56,11 +56,11 @@ jQuery(document).ready(function($) {
 			offset: '70%',
 			triggerOnce: true
 		});
-		
+
 	/* ==============================================
 		MMENU
-	 =============================================== */	
-   
+	 =============================================== */
+
 	function mmenuw() {
 		var wi = $(window).width();
 		var nb = $("#mmenu-side-menu").length;
@@ -77,7 +77,7 @@ jQuery(document).ready(function($) {
 			}
 		}
 	}
-	
+
 	function mmenuw_class() {
 		var wi = $(window).width();
 		if(wi < 992) {
@@ -88,46 +88,46 @@ jQuery(document).ready(function($) {
 			$('#nav .container-fluid').removeClass('container-fluid');
 		}
 	}
-		
+
 	mmenuw();
 	mmenuw_class();
-	$(window).resize(function() { 
+	$(window).resize(function() {
 		mmenuw();
 		mmenuw_class();
 	});
-   
+
 	/* ==============================================
 		Count Factors
 	 =============================================== */
-	 
+
 	 (function($) {
 		$.fn.countTo = function(options) {
 			// merge the default plugin settings with the custom options
 			options = $.extend({}, $.fn.countTo.defaults, options || {});
-	
+
 			// how many times to update the value, and how much to increment the value on each update
 			var loops = Math.ceil(options.speed / options.refreshInterval),
 				increment = (options.to - options.from) / loops;
-	
+
 			return $(this).each(function() {
 				var _this = this,
 					loopCount = 0,
 					value = options.from,
 					interval = setInterval(updateTimer, options.refreshInterval);
-	
+
 				function updateTimer() {
 					value += increment;
 					loopCount++;
 					$(_this).html(value.toFixed(options.decimals));
-	
+
 					if (typeof(options.onUpdate) == 'function') {
 						options.onUpdate.call(_this, value);
 					}
-	
+
 					if (loopCount >= loops) {
 						clearInterval(interval);
 						value = options.to;
-	
+
 						if (typeof(options.onComplete) == 'function') {
 							options.onComplete.call(_this, value);
 						}
@@ -135,7 +135,7 @@ jQuery(document).ready(function($) {
 				}
 			});
 		};
-	
+
 		$.fn.countTo.defaults = {
 			from: 0,  // the number the element should start at
 			to: 100,  // the number the element should end at
@@ -145,10 +145,10 @@ jQuery(document).ready(function($) {
 			onUpdate: null,  // callback method for every time the element is updated,
 			onComplete: null,  // callback method for when the element finishes updating
 		};
-	})(jQuery);	
-	 
-	 function countUp() {	
-			var dataperc;	
+	})(jQuery);
+
+	 function countUp() {
+			var dataperc;
 			$('.fact-number').each(function(){
 				dataperc = $(this).attr('data-perc'),
 				$(this).find('.factor').delay(6000).countTo({
@@ -156,10 +156,10 @@ jQuery(document).ready(function($) {
 					to: dataperc,
 					speed: 1000,
 					refreshInterval: 10,
-				});  
+				});
 			});
 		}
-		
+
 	$('.fact-number').waypoint(function() {
 		countUp();
 	},
@@ -167,10 +167,10 @@ jQuery(document).ready(function($) {
 		offset: '70%',
 		triggerOnce: true
 	});
-	   
+
    /* ==============================================
 		BUTTON TO TOP
-	=============================================== */	
+	=============================================== */
 	// fade in #back-top
 	$(function () {
 		// scroll body to 0px on click
@@ -181,31 +181,47 @@ jQuery(document).ready(function($) {
 			return false;
 		});
 	});
-	
+
 	/* ==============================================
 		OWL CAROUSEL
 	=============================================== */
-	
+
 	$("#slides ul").owlCarousel({
-		navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		nav: true,
-      	animateOut: 'fadeOut',
-      	animateIn: 'fadeIn',
+		// navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+		nav: false,
+      	// animateOut: 'fadeOut',
+      	// animateIn: 'fadeIn',
 		items:1,
 		margin:0,
 		loop:true,
-		autoplay:true,
+		autoplay:false,
 		autoplayTimeout:5000,
 		autoplayHoverPause:false,
 		dots: false,
 		stagePadding:0,
 		smartSpeed:1000,
 	});
-	
+
+	// $("#slides ul").owlCarousel({
+	// 	navText: ['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+	// 	nav: true,
+ //      	animateOut: 'fadeOut',
+ //      	animateIn: 'fadeIn',
+	// 	items:1,
+	// 	margin:0,
+	// 	loop:true,
+	// 	autoplay:true,
+	// 	autoplayTimeout:5000,
+	// 	autoplayHoverPause:false,
+	// 	dots: false,
+	// 	stagePadding:0,
+	// 	smartSpeed:1000,
+	// });
+
 	$("#quote-slider").owlCarousel({
 	  	items: 1,
 	  });
-	
+
 	$("#clients-slider").owlCarousel({
 	   	dots:false,
 	  	responsive:{
@@ -229,8 +245,8 @@ jQuery(document).ready(function($) {
 			}
 		}
 	  });
-   
- 
+
+
   $("#blog-slider").owlCarousel({
 	  dots:true,
 	  nav: false,
@@ -252,7 +268,7 @@ jQuery(document).ready(function($) {
 			}
 		}
 	  });
-	
+
 	 /* ==============================================
 		TOOLTIP
 	=============================================== */
@@ -260,55 +276,55 @@ jQuery(document).ready(function($) {
 		container: 'body'
 	});
 
-   
+
    /* ==============================================
 		MIXITUP PORTFOLIO
-	=============================================== */	
+	=============================================== */
 	//Mixitup
 	$(function(){
-    	$('.home-projects').mixItUp();  
+    	$('.home-projects').mixItUp();
 	});
-	
+
 	/* ==============================================
 		MAGNIFIC POPUP
-	=============================================== */	
+	=============================================== */
 	$('.popup').magnificPopup({
 		type: 'image',
 		gallery: {
 			enabled: true
 		},
     });
- 
+
  	/* ==============================================
 		PARALLAX
-	=============================================== */	
-	
+	=============================================== */
+
 	// Only load parallax when not on mobile devices
 	if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
 		$('.image1').parallax("0%", -0.3);
 		$('.image2').parallax("0%", -0.3);
-		$('.image3').parallax("0%", -0.3); 
+		$('.image3').parallax("0%", -0.3);
 	}
 
-	
+
 	/* ==============================================
 		NAV
-	=============================================== */	
+	=============================================== */
 	$("#nav").sticky({
 		topSpacing: 0
 	});
-	
+
 	$('.home-nav').onePageNav({
 		scrollSpeed: 1200,
 		currentClass: 'active',
 		changeHash: true,
 		filter: ':not(.external)'
 	});
-	
-	
+
+
 	/* ==============================================
 		AJAX CONTACT FORM
-	=============================================== */	
+	=============================================== */
 	$('#contactform').submit(function(){
 
 		var action = $(this).attr('action');
@@ -342,20 +358,20 @@ jQuery(document).ready(function($) {
 
 	});
 
-	
+
 });
 //End Document.ready
 
-	
+
 $(window).load(function() {
 	'use strict';
-	
+
 	/* ==============================================
 		PAGE LOADER
 	=============================================== */
-	
+
 	$(".loader-item").delay(7000).fadeOut();
 	$("#pageloader").delay(12000).fadeOut("slow");
-	
+
 });
 //End window.load
